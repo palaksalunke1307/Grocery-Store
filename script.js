0b0e47bf0a037b8161d03123ae13872b8ed73ceb
palaksalunke1307 <palaksalunke1307@gmail.com>
2025-02-15 17:10:06 +0530

    Add User Login Form

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ document.querySelector('#search-btn').onclick = () =>
 {
   searchForm.classList.toggle('active');
 }
+
+
+let loginForm=document.querySelector('.login-form');
+
+document.querySelector('#login-btn').onclick = () =>
+{
+  loginForm.classList.toggle('active');
+}
+
